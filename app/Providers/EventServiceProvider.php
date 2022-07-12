c6ff08d7478df42f53c1027e5f1dfbf9a68738e4
ahmadrezagh <ahmadreza.web@gmail.com>
2022-07-12 23:19:00 +0430

    register observers and call seeders

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,10 +2,13 @@
 
 namespace App\Providers;
 
+use App\Models\Transaction;
+use App\Models\Transfer;
+use App\Observers\TransactionObserve;
+use App\Observers\TransferObserve;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -27,7 +30,8 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Transaction::observe(TransactionObserve::class);
+        Transfer::observe(TransferObserve::class);
     }
 
     /**
