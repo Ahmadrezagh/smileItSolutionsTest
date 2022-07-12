c2a8854fd1c7cdaddd8b3cb3630ea7076bd1a585
ahmadrezagh <ahmadreza.web@gmail.com>
2022-07-12 23:48:39 +0430

    remove sanctum package

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,12 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 use Silber\Bouncer\Database\HasRolesAndAbilities;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
     use HasRolesAndAbilities;
 
     protected $fillable = [
