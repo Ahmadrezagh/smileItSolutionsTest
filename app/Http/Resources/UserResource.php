ca198608e171212c562d6537cbebe0460ee7c942
ahmadrezagh <ahmadreza.web@gmail.com>
2022-07-14 17:37:08 +0430

    Update user resource and id and use BaseResource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Resources;
 
-use Illuminate\Http\Resources\Json\JsonResource;
-
-class UserResource extends JsonResource
+class UserResource extends BaseResource
 {
     /**
      * Transform the resource into an array.
@@ -15,6 +13,7 @@ class UserResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->resource->id,
             'first_name' => $this->resource->first_name,
             'last_name' => $this->resource->last_name,
             'email' => $this->resource->email,
