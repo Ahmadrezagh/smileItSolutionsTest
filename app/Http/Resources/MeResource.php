446f9d4710a6bb17154063bb828e6739da9d485b
ahmadrezagh <ahmadreza.web@gmail.com>
2022-07-14 17:48:28 +0430

    Resource for User information

---
+++ app/Http/Resources/MeResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'first_name' => $this->resource->first_name,
+            'last_name' => $this->resource->last_name,
+            'email' => $this->resource->email,
+            'roles' => $this->resource->getRoles(),
+        ];
+    }
+}
