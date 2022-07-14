488f3b5431102ced1f12a5f94876f13d36eca993
ahmadrezagh <ahmadreza.web@gmail.com>
2022-07-14 17:36:41 +0430

    Get history of specific account card number

---
+++ app/Http/Requests/Api/V1/Transfer/IndexTransferRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\Api\V1\Transfer;
+
+use App\Http\Requests\PaginateAndOrderableAndSearchableRequest;
+use Illuminate\Validation\Rule;
+
+class IndexTransferRequest extends PaginateAndOrderableAndSearchableRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules():array
+    {
+        return [
+            'card_number' => ['nullable',Rule::exists('accounts','card_number')]
+        ];
+    }
+
+
+    public function orderBy(): array
+    {
+        return ['amount', 'created_at', 'updated_at'];
+    }
+}
