fde4d381cd1b2f2b962f112f2af0847f5472fcf3
ahmadrezagh <ahmadreza.web@gmail.com>
2022-07-14 17:32:17 +0430

    Account requests,soft delete

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -2,14 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Account extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $with = ['user'];
     protected $fillable = [
         'user_id',
@@ -31,5 +33,21 @@ class Account extends Model
     public function setPasswordAttribute($value)
     {
         $this->attributes['password'] = bcrypt($value);
+    }
+
+    public function scopeFilterByUserEmail(Builder $builder, $user_email = null)
+    {
+        if($user_email)
+        {
+            $builder->whereHas('user',function (Builder $query) use ($user_email) {
+               $query->where('email','=',$user_email);
+            });
+        }
+        return $builder;
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'card_number';
     }
 }
